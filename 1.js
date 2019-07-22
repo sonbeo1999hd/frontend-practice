1b50c1334a1062580fdf5a9f0c6b6c17a24f70ab
sonbeo1999hd <43996384+sonbeo1999hd@users.noreply.github.com>
2019-07-23 00:54:02 +0700

    update một số hiệu ứng js + fix css

---
+++ 1.js
@@ -0,0 +1,64 @@
+// paralax navbar
+
+var trangthai = false;
+var small_navbar = document.querySelector('.nav');
+
+window.addEventListener('scroll', function () {
+    if (this.window.pageYOffset > 120) {
+        if (trangthai == true) {
+            small_navbar.classList.add('smaller');
+            trangthai = false;
+        }
+
+    } else {
+        if (trangthai == false) {
+            small_navbar.classList.remove('smaller');
+            trangthai = true;
+        }
+    }
+});
+
+
+// scroll smooth when click on navbar 
+
+var link = document.querySelectorAll('.group1 .nav li a')
+// console.log(link);
+for (let i = 0; i < link.length; i++) {
+    link[i].addEventListener('click', function (e) {
+        let temp = link[i].getAttribute('href');
+        let target = document.querySelector(temp);
+        // console.log(target);
+        target.scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        })
+        // history.pushState(null, null, temp)
+        e.preventDefault()
+    })
+
+}
+
+// slide_show when click icon in group5
+var slideIndex = 1;
+showSlides(slideIndex);
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+
+}
